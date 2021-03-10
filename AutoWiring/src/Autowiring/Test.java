79e3e25d3fead583acf1f2274b7e02f698ee5910
akshatakarsh <akshatakarsh1@gmail.com>
2021-03-10 22:30:04 +0530

    It is used to add implicit dependency injection or we have not to add tag ref bean for adding dependent object

---
+++ AutoWiring/src/Autowiring/Test.java
@@ -0,0 +1,16 @@
+package Autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ ApplicationContext con= new ClassPathXmlApplicationContext("applicationContext.xml");
+ A a=(A)con.getBean("a", A.class);
+		a.display();
+		
+	}
+
+}
