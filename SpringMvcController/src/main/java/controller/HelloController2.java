8ea7894883934e8a83e470ab3df2f41a83e3724a
akshatakarsh <akshatakarsh1@gmail.com>
2021-03-24 21:31:42 +0530

    This is MVC framework which has controller example showing controller demo example of Spring Framework

---
+++ SpringMvcController/src/main/java/controller/HelloController2.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController2 {
+	@RequestMapping("/hello2")
+public String display() {
+	return "viewpage2";
+}
+}
